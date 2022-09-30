1a66eabc7f57625be7d6984d1c862bcb0363c2ef
Lab Computo <Computosala42022@ugb.edu.sv>
2022-09-30 12:05:27 -0600

    r

--- Semana10/DAL/Database.cs
+++ Semana10/DAL/Database.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Semana10.Properties;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace Semana10.DAL
 {
-    class Database
+    public class Database
     {
+        public static string getStrConnection()
+        {
+            return Settings.Default.empleadosConnectionString;
+        }
+
+        public SqlConnection GetConnection()
+        {
+           SqlConnection Con = new SqlConnection(getStrConnection());
+            return Con;
+        }
+
     }
 }
